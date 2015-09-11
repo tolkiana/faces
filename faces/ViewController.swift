61288e2982a5567d8bde74d695ad16395de4f6cc
Nelida Velazquez <nelida_v@hotmail.com>
2015-09-10 23:37:20 -0400

    Save and load image

--- faces/ViewController.swift
+++ faces/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imageView:UIImageView!
     
     let imagePicker = UIImagePickerController()
+    let kMyCurrentFace = "myFace.png"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        loadCurrentFace()
     }
     
     // MARK: - IBActions
@@ -45,6 +47,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    func saveImage(image : UIImage){
+        let imageData = NSData(data: UIImagePNGRepresentation(image))
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        var docs: String = paths[0] as! String
+        let fullPath = docs.stringByAppendingPathComponent(kMyCurrentFace)
+        let result = imageData.writeToFile(fullPath, atomically: true)
+    }
+    
+    func imageWithName(imageName: String) -> UIImage? {
+    
+        let documentDirectory = NSSearchPathDirectory.DocumentDirectory
+        let userDomainMask = NSSearchPathDomainMask.UserDomainMask
+        
+        if let paths = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true) {
+            if paths.count > 0 {
+                if let dirPath = paths[0] as? String {
+                
+                    let readPath = dirPath.stringByAppendingPathComponent(imageName)
+                    let image = UIImage(contentsOfFile: readPath)
+                    return image
+                }
+            }
+        }
+        return nil
+    }
+    
+    func loadCurrentFace() {
+    
+        if let myFace = imageWithName(kMyCurrentFace) {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = myFace
+        }
+    }
+    
     // MARK: - UIImagePickerControllerDelegate
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
@@ -52,6 +88,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .ScaleAspectFit
             imageView.image = pickedImage
+            saveImage(pickedImage)
         }
         dismissViewControllerAnimated(true, completion: nil)
         
