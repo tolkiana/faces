1cc8510e8838a807ca7d843b5d322331b4afd5e2
Nelida Velazquez <nelida_v@hotmail.com>
2015-10-15 22:53:36 -0400

    Improve algorithm to get a bigger portion of the face

--- faces/ViewController.swift
+++ faces/ViewController.swift
@@ -28,18 +28,31 @@ class ViewController: UIViewController {
         imageView.image!.drawInRect(CGRectMake(0,0,imageView.image!.size.width,imageView.image!.size.height))
         
         for feature in features{
+            let face = feature as! CIFaceFeature
             
             //context
             let drawCtxt = UIGraphicsGetCurrentContext()
             
             //face
             var faceRect = (feature as! CIFaceFeature).bounds
-            faceRect.origin.y = imageView.image!.size.height - faceRect.origin.y - faceRect.size.height
-            //faceRect = CGRectInset(faceRect, -20, -20)
+            faceRect.origin.y = imageView.image!.size.height - faceRect.origin.y - faceRect.size.height - 60.0
+            faceRect.size.height = faceRect.size.height + 60.0
+            let faceWidht = faceRect.size.height
+            
+            faceView.image = croppIngimage(imageView.image!, toRect: faceRect)
+            
+            if face.hasLeftEyePosition && face.hasRightEyePosition {
+                let middle = (face.leftEyePosition.x + face.rightEyePosition.x) / 2
+                
+                faceRect.origin.x = middle - faceWidht/2
+                faceRect.size.width = faceWidht
+            }
+            
+            
             CGContextSetStrokeColorWithColor(drawCtxt, UIColor.redColor().CGColor)
             CGContextStrokeRect(drawCtxt,faceRect)
             
-            faceView.image = croppIngimage(imageView.image!, toRect: faceRect)
+            print(faceRect)
             
         }
         let drawedImage = UIGraphicsGetImageFromCurrentImageContext()
