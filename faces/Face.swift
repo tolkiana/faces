271a411eaef9f357eb820edfec43beec14e3f3db
velazqn <tolkiana@gmail.com>
2015-11-09 20:57:40 -0500

    Created face struct

---
+++ faces/Face.swift
@@ -0,0 +1,15 @@
+//
+//  Face.swift
+//  faces
+//
+//  Created by Nelida Velazquez (IT - IS Ecomm O) on 11/9/15.
+//  Copyright © 2015 Nelida Velázquez. All rights reserved.
+//
+
+import Foundation
+
+struct Face {
+    var title: String
+    var emoticon: String
+    var imageName: String
+}
