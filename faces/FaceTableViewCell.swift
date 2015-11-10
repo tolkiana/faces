5981959436c6cc7f2d8a1f69bfad23de1f21dd80
velazqn <tolkiana@gmail.com>
2015-11-09 20:34:15 -0500

    Created Custom table view cell for face

---
+++ faces/FaceTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FaceTableViewCell.swift
+//  faces
+//
+//  Created by Nelida Velazquez (IT - IS Ecomm O) on 11/9/15.
+//  Copyright © 2015 Nelida Velázquez. All rights reserved.
+//
+
+import UIKit
+
+class FaceTableViewCell: UITableViewCell {
+
+    @IBOutlet var faceTitleLabel: UILabel!
+    @IBOutlet var emoticonLabel: UILabel!
+
+}
