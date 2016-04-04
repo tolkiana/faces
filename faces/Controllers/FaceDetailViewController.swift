b69e7cf9989bc977f9857d6c9ee8d604fb2f0bcd
nelidaVelazquez <nelida.velazquez@dominos.com>
2016-04-03 22:15:37 -0400

    Add DetailFaceViewController

---
+++ faces/Controllers/FaceDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FaceDetailViewController.swift
+//  faces
+//
+//  Created by Nelida Velazquez on 4/3/16.
+//  Copyright © 2016 Nelida Velázquez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FaceDetailViewController: UIViewController {
+    
+    @IBOutlet var lastEmoji: UILabel!
+    @IBOutlet var contactName: UILabel!
+    @IBOutlet var emoji: UILabel!
+    @IBOutlet var faceImageView: UIImageView!
+}
