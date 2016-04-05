71b53691aa0e34d464d8e7fa80f98482566bc843
nelidaVelazquez <nelida.velazquez@dominos.com>
2016-04-04 22:50:43 -0400

    Add new method

--- facesKit/FSImageUtility.swift
+++ facesKit/FSImageUtility.swift
@@ -64,6 +64,22 @@ class FSImageUtility {
         return faces;
     }
     
+    func maskRoundedImage(image: UIImage, radius: Float) -> UIImage {
+        let imageView: UIImageView = UIImageView(image: image)
+        var layer: CALayer = CALayer()
+        layer = imageView.layer
+        
+        layer.masksToBounds = true
+        layer.cornerRadius = CGFloat(radius)
+        
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let roundedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return roundedImage
+    }
+    
     // MARK: Private methods
     
     private func featuresInImage(image: UIImage) -> [CIFeature] {
